728bb8314c6b10757661ccfa69711399dcf1d238
akhil2109kumar <akhil2109kumar@gmail.com>
2024-01-29 12:47:28 +0530

    Tailwind installed

--- frontend/movies/src/pages/homepage/Homepage.tsx
+++ frontend/movies/src/pages/homepage/Homepage.tsx
@@ -3,6 +3,91 @@ import React from "react";
 const HomePage = () => {
   return (
     <>
+      <div className="bg-black text-white min-h-screen flex flex-col">
+        {/* <Headers /> */}
+        <nav className="p-4"></nav>
+        <main className="flex-grow flex flex-col items-center justify-center p-4">
+          <h1 className="text-4xl font-sans mb-4">
+            Need help finding the next movie?
+          </h1>
+          <p className="mb-4">
+            Search for your next movie through HelloMovie's huge movie library
+          </p>
+          <div className="flex items-center mb-4">
+            <label
+              htmlFor="default-search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                <svg
+                  className="w-3 h-3 text-gray-500 dark:text-yellow-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </div>
+              <input
+                type="search"
+                id="default-search"
+                className="block w-full p-3 ps-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Search Mockups, Logos..."
+              />
+            </div>
+          </div>
+          <div className="flex">
+            <div className="flex self-center mx-1 ">
+              <svg
+                className="w-4 h-4 text-gray-800 dark:text-white"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="yellow"
+                viewBox="0 0 10 16"
+              >
+                <path
+                  stroke="yellow"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M7.293 1.707 1.707 7.293a1 1 0 0 0 0 1.414l5.586 5.586A1 1 0 0 0 9 13.586V2.414a1 1 0 0 0-1.707-.707Z"
+                />
+              </svg>
+            </div>
+            <div className="mr-5">
+              <button>Movies</button>
+            </div>
+
+            <div className="flex self-center mx-1 ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="yellow"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125Z"
+                />
+              </svg>
+            </div>
+            <div>TV Shows</div>
+          </div>
+        </main>
+      </div>
     </>
   );
 };
