e1fc223b46b2e5bdbaa96b0abc1d973737c62427
akhil2109kumar <akhil2109kumar@gmail.com>
2024-01-29 12:39:54 +0530

    routing added

--- frontend/movies/src/App.tsx
+++ frontend/movies/src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./pages/homepage/Homepage";
 
 const App = () => {
   return (
     <div>
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
